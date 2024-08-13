b846dbaae05f50fffc2344e153876deef04f8d00
Your NameMayssan <mayssanashkar5@gmail.com>
2024-08-13 15:33:34 +0300

    added /data

--- servv.js
+++ servv.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const PORT = process.env.PORT || 3030;
 const app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+app.get('/data', function (req, res) {
+    var data = [
+        {id:11 , name:'rana'},
+        {id:12 , name:'rama'},
+        {id:13 , name:'mysn'}
 
-app.listen(3000)
+    ]
+    res.send(data)
+  })
+
+app.listen(PORT)
